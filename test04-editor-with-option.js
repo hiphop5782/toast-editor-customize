35e4739c46a76fd94535ef75ebc20336701c9665
hiphop5782 <zdlsz@hanmail.net>
2020-06-29 19:03:33 +0900

    editor with plugin

--- test04-editor-with-option.js
+++ test04-editor-with-option.js
@@ -1,12 +1,35 @@
-(function(w){
-    
-    if(!w) throw "Window is not defined";
-    if(!toastui || !toastui.Editor) throw "Toast Dependency Not Defined";
+(function (w) {
+
+    if (!w) throw "Window is not defined";
+    if (!toastui || !toastui.Editor) throw "Toast Dependency Not Defined";
 
     w.Hacademy = w.Hacademy || {};
     w.Hacademy.TuiEditor = w.Hacademy.TuiEditor || {};
 
     var util = w.Hacademy.TuiEditor;
+
+    util.clone = function clone(obj) {
+        if (obj === null || typeof (obj) !== 'object')
+            return obj;
+
+        var copy = obj.constructor();
+
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+                copy[attr] = clone(obj[attr]);
+            }
+        }
+
+        return copy;
+    }
+
+    const {chart, tableMergedCell, uml, colorSyntax, codeSyntaxHighlight} = toastui.Editor.plugin;
+    const chartOptions = {
+        minWidth: 100,
+        maxWidth: 600,
+        minHeight: 100,
+        maxHeight: 300
+    };
 
     //editor 생성 함수
     // - editor를 생성할 때 내부에 있는 글자를 value로 설정하도록 구현
@@ -16,60 +39,58 @@
     // .wysiwyg-editor : wysiwyg 모드로 실행되도록 변경
     var defaultEditorOptions = {
         //대상은 생성 시 지정
-        el : null,
+        el: null,
 
         //기본 표시 형태를 markdown 모드로 설정
-        initialEditType:"markdown",
-        
+        initialEditType: "markdown",
+
         //높이 설정
-        height:"400px",
-        
+        height: "400px",
+
         //통계 자동 전송 거부
-        usageStatistics:false,
+        usageStatistics: false,
 
         //미리보기 스타일을 horizontal 모드로 설정
-        previewStyle:"horizontal",
+        previewStyle: "horizontal",
 
         //툴바 스타일 설정
         toolbarItems: [
-            'heading',
-            'bold',
-            'italic',
+            'heading','bold','italic',
             'divider',
-            'hr',
-            'quote',
+            'hr','quote',
             'divider',
-            'ul',
-            'ol',
-            'task',
-            'indent',
-            'outdent',
+            'ul','ol','task','indent','outdent',
             'divider',
-            'table',
-            'image',
-            'link',
+            'table','image','link',
             'divider',
-            'code',
-            'codeblock',
+            'code','codeblock',
             'divider'
+        ],
+
+        plugins: [
+            [chart, chartOptions]//chart plugin
+            ,uml//uml plugin
+            ,codeSyntaxHighlight//code highlight plugin
+            ,tableMergedCell//table cell plugin
+            ,colorSyntax//color syntax plugin
         ],
     };
 
-    util.createEditor = function(selector){
+    util.createEditor = function (selector) {
         var elements = document.querySelectorAll(selector);
-        if(!elements.length) return;
+        if (!elements.length) return;
 
         util.editors = util.editors || [];
-        for(var i=0; i < elements.length; i++){
+        for (var i = 0; i < elements.length; i++) {
             //ID 계산(없으면 i로 설정)
             var idx = elements[i].id || i;
 
             //Option 복제 및 설정
-            var cloneOptions = JSON.parse(JSON.stringify(defaultEditorOptions));
+            var cloneOptions = util.clone(defaultEditorOptions);
 
             //대상 설정
             cloneOptions.el = elements[i];
-            
+
             //내용을 불러와서 설정
             var content = elements[i].textContent || "";
             elements[i].textContent = "";
@@ -80,14 +101,15 @@
 
             // .vertical-editor : 미리보기가 같이 나오도록 변경
             // .wysiwyg-editor : wysiwyg 모드로 실행되도록 변경
-            if(classList.contains("vertical-editor"))
+            if (classList.contains("vertical-editor"))
                 cloneOptions.previewStyle = "vertical";
-            if(classList.contains("wysiwyg-editor"))
+            if (classList.contains("wysiwyg-editor"))
                 cloneOptions.initialEditType = "wysiwyg";
 
             //create editor and push
+            console.log(cloneOptions);
             util.editors[idx] = new toastui.Editor(cloneOptions);
         }
     };
-        
+
 })(window);
