53f6ca333ce8bbbf5797d6557e5c19521bcc2407
hiphop5782 <zdlsz@hanmail.net>
2020-08-27 21:56:52 +0900

    fix

--- js/toast-custom-htmlrenderer-plugin.js
+++ js/toast-custom-htmlrenderer-plugin.js
@@ -4,8 +4,8 @@
     creator : Hacademy(www.sysout.co.kr)
 */
 function htmlRendererPlugin(){
-    toastui.Editor.codeBlockManager.setReplacer("html-render", function (htmlCode) {
-        const wrapperId = 'html-render' + Math.random().toString(36).substr(2, 10);
+    toastui.Editor.codeBlockManager.setReplacer("render", function (htmlCode) {
+        const wrapperId = 'render' + Math.random().toString(36).substr(2, 10);
         setTimeout(renderHtml.bind(null, wrapperId, htmlCode), 0);
         return '<div id=' + wrapperId + ' data-content="'+htmlCode+'"></div>';
     });
