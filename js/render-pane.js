5e4a753e9c2bc312375b4c61357d7c88359698eb
hiphop5782 <zdlsz@hanmail.net>
2020-08-27 20:19:10 +0900

    render pane finish

---
+++ js/render-pane.js
@@ -0,0 +1,160 @@
+(function(w){
+    if(!w) throw "Window not defined";
+    w.Hacademy = w.Hacademy || {};
+    w.Hacademy.RenderPane = function(el, options){
+        if(typeof el === "string")
+            el = document.querySelector(el);
+        
+        this.el = el;
+        if(!this.el.classList.contains("render-pane")){
+            this.el.classList.add("render-pane");
+        }
+
+        var defaultOptions = {
+            split:false,        //enable split mode
+            mode:"result",  //result or source
+            edit:false,         //enable edit mode
+        };
+
+        this.options = Object.assign({}, defaultOptions, options);
+
+        this.createRenderPane();
+        
+        this.displayContent();
+        
+        //split 모드가 아닐 경우 mode 설정
+        if(!this.options.split){
+            this.tabChange(this.options.mode);
+        }
+        else{
+            this.el.classList.add("split-pane");
+            this.renderContent();
+        }
+    }
+
+    w.Hacademy.RenderPane.prototype.createRenderPane = function(){
+        var app = this;
+
+        //create header
+        var headerDiv = document.createElement("div");
+        headerDiv.classList.add("tab-header");
+
+        var sourceTitleDiv = document.createElement("div");
+        sourceTitleDiv.classList.add("tab-title");
+
+        var span = document.createElement("span");
+        span.textContent = "source";
+        sourceTitleDiv.appendChild(span);
+        
+        var resultTitleDiv = document.createElement("div");
+        resultTitleDiv.classList.add("tab-title");
+        span = document.createElement("span");
+        span.textContent = "result";
+        resultTitleDiv.appendChild(span);
+        
+        headerDiv.appendChild(sourceTitleDiv);
+        headerDiv.appendChild(resultTitleDiv);
+
+        //create body
+        var bodyDiv = document.createElement("div");
+        bodyDiv.classList.add("tab-body");
+
+        var sourceContentDiv = document.createElement("div");
+        sourceContentDiv.classList.add("tab-content");
+
+        var resultContentDiv = document.createElement("div");
+        resultContentDiv.classList.add("tab-content");
+
+        bodyDiv.appendChild(sourceContentDiv);
+        bodyDiv.appendChild(resultContentDiv);
+        
+        this.el.appendChild(headerDiv);
+        this.el.appendChild(bodyDiv);
+
+        if(!this.options.split){
+            sourceTitleDiv.addEventListener("click", function(){
+                app.tabChange("source");
+            });
+            resultTitleDiv.addEventListener("click", function(){
+                app.tabChange("result");
+            });
+        }
+    };
+
+    w.Hacademy.RenderPane.prototype.displayContent = function(){
+        var content = this.el.dataset.content || "";
+        if(!content) return;
+
+        this.content = content;
+
+        this.el.removeAttribute("data-content");
+
+        var pre = document.createElement("pre");
+        var code = document.createElement("code");
+        code.classList.add("html");
+        code.textContent = content;
+
+        pre.appendChild(code);
+
+        var contents = this.el.querySelectorAll(".tab-content");
+        contents[0].textContent = "";
+        contents[0].appendChild(pre);
+
+        hljs.highlightBlock(code);
+    };
+
+    w.Hacademy.RenderPane.prototype.renderContent = function(){
+        if(!this.content) return;
+
+        var iframe = this.el.querySelector("iframe") || document.createElement("iframe");
+
+        //iframe 생성
+        this.el.querySelectorAll(".tab-content")[1].appendChild(iframe);                
+
+        //내용 작성
+        var iframedoc = iframe.contentDocument || iframe.contentWindow.document;
+        iframedoc.open();
+        iframedoc.write(this.content);
+        iframedoc.close();                
+
+        iframe.addEventListener("load", function(){
+            var frame = this;
+            setTimeout(function(){
+                frameWindow = frame.contentWindow;
+                frameBody = frame.contentWindow.document.body;
+                
+                var minHeight = 150;
+                var fixOffset = 30;
+                var height = Math.max(parseInt(frameWindow.getComputedStyle(frameBody).height) || minHeight, minHeight) + fixOffset;
+                frame.style.height = height + "px";
+            }, 10);
+        });
+
+        //onload 이벤트 강제 발생
+        iframe.dispatchEvent(new Event("load"));
+    };
+
+    w.Hacademy.RenderPane.prototype.tabChange = function(mode){
+        mode = mode || "result";
+        if(this.curMode === mode) return;
+        
+        var titles = this.el.querySelectorAll(".tab-title");
+        var contents = this.el.querySelectorAll(".tab-content");
+        if(mode == "source"){
+            titles[0].classList.add("active");
+            titles[1].classList.remove("active");
+            contents[0].classList.add("active");
+            contents[1].classList.remove("active");
+        }
+        else{
+            titles[0].classList.remove("active");
+            titles[1].classList.add("active");
+            contents[0].classList.remove("active");
+            contents[1].classList.add("active");
+
+            this.renderContent();
+        }
+
+        this.curMode = mode;
+    };
+})(window);
--- js/toast-custom-htmlrenderer-plugin.js
+++ js/toast-custom-htmlrenderer-plugin.js
@@ -4,116 +4,16 @@
     creator : Hacademy(www.sysout.co.kr)
 */
 function htmlRendererPlugin(){
-    toastui.Editor.codeBlockManager.setReplacer("render", function (htmlCode) {
-        const wrapperId = 'render' + Math.random().toString(36).substr(2, 10);
+    toastui.Editor.codeBlockManager.setReplacer("html-render", function (htmlCode) {
+        const wrapperId = 'html-render' + Math.random().toString(36).substr(2, 10);
         setTimeout(renderHtml.bind(null, wrapperId, htmlCode), 0);
-
-        return '<div id=' + wrapperId + '></div>';
+        return '<div id=' + wrapperId + ' data-content="'+htmlCode+'"></div>';
     });
     
     function renderHtml(wrapperId, htmlCode){
 		var el = document.getElementById(wrapperId);
-		var iframe = document.createElement('iframe');
-//		스크롤 제거를 위한 overflow 설정
-		iframe.scrolling = "no"
-		iframe.style.width = "100%";
-		iframe.style.minHeight = "150px";
-		iframe.style.overflow = "hidden";
-		iframe.style.border = "none";
-		iframe.addEventListener("load", function(){
-			resizeFrame();
+		var app = new Hacademy.RenderPane(el, {
+			mode:"result"
 		});
-//		iframe 배치
-		el.appendChild(iframe);
-		
-//		내용 작성
-		var iframedoc = iframe.contentDocument || iframe.contentWindow.document;
-		iframedoc.open();
-		iframedoc.write(htmlCode);
-		iframedoc.close();
-		
-//		onload 이벤트 발생
-		iframedoc.dispatchEvent(new Event("load"));
-		
-//		expand button
-//		var ex = createExpandButton();
-//		ex.addEventListener("click", expandFrame);
-//		el.appendChild(ex);
-		
-//		resize button
-//		var a = createResizeButton();
-//		a.addEventListener("click", function(){
-//			resizeFrame();
-//		});
-//		el.appendChild(a);
-		
-		function resizeFrame(){
-			var iframedoc = iframe.contentDocument || iframe.contentWindow.document;
-			var height = parseInt(iframe.contentWindow.getComputedStyle(iframedoc.body).height);
-			iframe.style.height = height+30+"px"; 
-		}
-		
-		function expandFrame(){
-			var wrapper = this.parentElement;
-			var iframe = wrapper.children[0];
-			var iframedoc = iframe.contentDocument || iframe.contentWindow.document;
-			var refresh = wrapper.children[2];
-			wrapper.removeAttribute("style");
-			if(wrapper.classList.contains("fullscreen")){
-				wrapper.style.position="relative";
-				wrapper.classList.remove("fullscreen");
-				this.classList.add("fa-expand");
-				this.classList.remove("fa-compress")
-				setTimeout(function(){
-					refresh.click();
-				}, 50);
-			}
-			else{
-				wrapper.style.position = "fixed";
-				wrapper.style.top = 0;
-				wrapper.style.bottom = 0;
-				wrapper.style.left = 0;
-				wrapper.style.right = 0;
-				wrapper.style.zIndex = 99999;
-				wrapper.style.backgroundColor = "white";
-				wrapper.classList.add("fullscreen");
-				iframe.style.height = "";
-				this.classList.remove("fa-expand");
-				this.classList.add("fa-compress")
-			}
-		}
-        
-        //resize button
-        function createResizeButton(){
-            var tag = document.createElement("i");
-            tag.classList.add("fa");
-            tag.classList.add("fa-refresh");
-            tag.title = "뷰어 크기 조정";
-            tag.style.display = "inline-block";
-            tag.style.position = "absolute";
-            tag.style.textDecoration = "none";
-            tag.style.top = "0.5rem";
-            tag.style.right = "2.5rem";
-            tag.style.cursor = "pointer";
-            tag.style.color = "#666";
-            tag.style.border = "1px solid gray;"
-            return tag;
-        }
-        
-        function createExpandButton(){
-            var tag = document.createElement("i");
-            tag.classList.add("fa");
-            tag.classList.add("fa-expand");
-            tag.title = "전체 화면 보기";
-            tag.style.display = "inline-block";
-            tag.style.position = "absolute";
-            tag.style.textDecoration = "none";
-            tag.style.top = "0.5rem";
-            tag.style.right = "0.5rem";
-            tag.style.cursor = "pointer";
-            tag.style.color = "#666";
-            tag.style.border = "1px solid gray;"
-            return tag;
-        }
 	}
 }
