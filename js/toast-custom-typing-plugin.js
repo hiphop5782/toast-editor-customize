789bd9984cb1946c0dcdad49cc0261f8f4d92192
hiphop5782 <zdlsz@hanmail.net>
2020-09-07 16:03:09 +0900

    error fix

--- js/toast-custom-typing-plugin.js
+++ js/toast-custom-typing-plugin.js
@@ -6,8 +6,8 @@
 function typingPlugin(){
     toastui.Editor.codeBlockManager.setReplacer("typing", function (sourceCode) {
         const wrapperId = 'typing' + Math.random().toString(36).substr(2, 10);
-        setTimeout(renderHtml.bind(null, wrapperId, sourceCode), 0);
-        return '<div id=' + wrapperId + ' data-source-code="'+encodeURIComponent(sourceCode)+'"></div>';
+        setTimeout(renderHtml.bind(null, wrapperId, sourceCode.trim()), 0);
+        return '<div id=' + wrapperId + ' data-source-code="'+encodeURIComponent(sourceCode.trim())+'"></div>';
     });
     
     function renderHtml(wrapperId, sourceCode){
